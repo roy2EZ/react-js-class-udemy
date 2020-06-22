4b1622ff835bcdb6b438db7770d230fe6ffa54c2
roy2EZ <41226742+roy2EZ@users.noreply.github.com>
2020-06-22 09:44:34 -0700

    state in stock data app

--- stock-data-app/src/Main.js
+++ stock-data-app/src/Main.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-function Main() {
-    return(
+function Main(props) {
+    return (
         <div>
             <h1>Stock Data For Today</h1>
             <ul>
-                <li>Amazon: value</li>
-                <li>Microsoft: value</li>
-                <li>Apple: value</li>
-                <li>Google: value</li>
+                <li>Amazon: {props.amazonValue}</li>
+                <li>Microsoft: {props.microsoftValue}</li>
+                <li>Apple: {props.appleValue}</li>
+                <li>Google: {props.googleValue}</li>
             </ul>
         </div>
     )
