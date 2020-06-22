7878fe3ba0894c023ceea284221a74a15fa29bf8
roy2EZ <41226742+roy2EZ@users.noreply.github.com>
2020-06-22 09:44:07 -0700

    state in stock data app

--- stock-data-app/src/App.js
+++ stock-data-app/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Welcome from './Welcome';
+import Main from './Main';
 
 class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      name: "Roy Chen",
+      microsoftValue: 100,
+      googleValue: 200,
+      amazonValue: 300,
+      appleValue: 150
+    }
+  }
+
+
   render() {
     return (
       <div>
-        Stock Data App
-        <Welcome name="Roy"/>
+        <h1>Stock Data App</h1>
+        <Welcome name={this.state.name} />
+        <Main microsoftValue={this.state.microsoftValue}
+          amazonValue={this.state.amazonValue}
+          googleValue={this.state.googleValue}
+          appleValue={this.state.appleValue}
+        />
       </div>
     )
   }
---
+++ stock-data-app/src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Main() {
+    return(
+        <div>
+            <h1>Stock Data For Today</h1>
+            <ul>
+                <li>Amazon: value</li>
+                <li>Microsoft: value</li>
+                <li>Apple: value</li>
+                <li>Google: value</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Main;
