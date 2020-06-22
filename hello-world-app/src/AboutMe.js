dae46cf440325880505a39ef3c49bb26c2d895b0
roy2EZ <41226742+roy2EZ@users.noreply.github.com>
2020-06-22 01:52:40 -0700

    create stock data app

---
+++ hello-world-app/src/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>I am Rongyi Chen</p>
+        </div>
+    )
+}
+
+export default AboutMe;
---
+++ hello-world-app/src/HelloWorld.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+            <ul>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+            </ul>
+        </div>
+    );
+}
+
+export default HelloWorld;
